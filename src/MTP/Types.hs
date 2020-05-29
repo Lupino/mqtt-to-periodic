feb85e14f0231b983fc99aef3c24ef6d21822bee
Lupino <lmjubuntu@gmail.com>
2020-05-29 13:01:57 +0800

    use hex on hashMsg

--- src/MTP/Types.hs
+++ src/MTP/Types.hs
@@ -10,8 +10,8 @@ module MTP.Types
 
 import           Crypto.Hash          (Digest, SHA256, hash)
 import           Data.Aeson           (ToJSON (..), encode, object, (.=))
-import           Data.Byteable        (toBytes)
 import           Data.ByteString.Lazy (ByteString, toStrict)
+import           Data.String          (fromString)
 import           Data.Text.Encoding   (decodeUtf8)
 import           Network.MQTT.Client  (Topic)
 import           Periodic.Types       (JobName (..), Workload (..))
@@ -32,7 +32,7 @@ hashMsg_ :: Msg -> Digest SHA256
 hashMsg_ = hash . toStrict . encode
 
 hashMsg :: Msg -> JobName
-hashMsg = JobName . toBytes . hashMsg_
+hashMsg = fromString . show . hashMsg_
 
 encodeMsg :: Msg -> Workload
 encodeMsg = Workload . toStrict . encode
